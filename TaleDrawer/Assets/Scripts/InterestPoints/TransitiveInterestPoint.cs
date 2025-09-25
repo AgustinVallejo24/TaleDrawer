76c74fefd3108622dd1e5261e4fd8ec4f16ce6df
Apri19 <nicoaprigliano19@gmail.com>
2025-09-25 13:02:42 -0300

    Empezando con el cambio de zona

---
+++ TaleDrawer/Assets/Scripts/InterestPoints/TransitiveInterestPoint.cs
@@ -0,0 +1,26 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class TransitiveInterestPoint : InterestPoint
+{
+    [SerializeField] Transform _target;
+    [SerializeField] CinemachineTargetGroup _cameraTarget;
+    [SerializeField] GameObject _currentZone;
+    [SerializeField] GameObject _nextZone;
+    [SerializeField] SpawnableManager _spawnableManager;
+    
+
+    protected override void Start()
+    {
+        base.Start();
+        pointEvent.AddListener(Transition);
+    }
+
+    void Transition()
+    {
+        _nextZone.SetActive(true);
+        _cameraTarget.Targets[0].Object = _target;
+        _spawnableManager.spawningPos = _target;
+        _currentZone.SetActive(false);
+    }
+}
